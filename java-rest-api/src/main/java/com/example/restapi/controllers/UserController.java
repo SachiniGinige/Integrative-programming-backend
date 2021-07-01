c741b6432d85cc8484e49cb28ea1c36a688b330c
sachini <sachiniginige@gmail.com>
2021-07-02 00:37:41 +0530

    edited user path

--- java-rest-api/src/main/java/com/example/restapi/controllers/UserController.java
+++ java-rest-api/src/main/java/com/example/restapi/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.restapi.controllers;
 import com.example.restapi.models.User;
 import com.example.restapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +25,16 @@ public class UserController {
 //    public User getUserById(@PathVariable String id){
 //        return userRepository.findUserByUser_id(id);
 //    }
+
+    @GetMapping("/users/{id}")
+    public User getUserByUserId(@PathVariable String id){
+        return userRepository.findByUser_id(id)
+                .orElseThrow(()->new RuntimeException(
+                        String.format("Cannot find user by User ID: %s",id)
+                ));
+    }
+
+
     @PostMapping("users/add")
     public User addUser(@RequestBody User user){
         return userRepository.save(user);
--- java-rest-api/src/main/java/com/example/restapi/repositories/UserRepository.java
+++ java-rest-api/src/main/java/com/example/restapi/repositories/UserRepository.java
@@ -2,9 +2,13 @@ package com.example.restapi.repositories;
 
 import com.example.restapi.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-//    User findUserByUser_id(String id);
+    @Query("{'user_id':?0}")
+    Optional<User> findByUser_id(String user_id);
 }
