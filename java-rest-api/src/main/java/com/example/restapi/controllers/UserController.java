652c4bc3ab026899fbecec328a327bce01dc3fb4
sachini <sachiniginige@gmail.com>
2021-07-02 12:05:48 +0530

    User controller enable cors

--- java-rest-api/src/main/java/com/example/restapi/controllers/UserController.java
+++ java-rest-api/src/main/java/com/example/restapi/controllers/UserController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class UserController {
     private  final UserRepository userRepository;
     private final CustomRepository customRepository;
