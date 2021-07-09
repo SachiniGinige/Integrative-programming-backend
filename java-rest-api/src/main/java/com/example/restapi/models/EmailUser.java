9758dae9e7cc395701b6ffc327829f1414db5cdf
sachini <sachiniginige@gmail.com>
2021-07-09 14:20:07 +0530

    moved send email
    moved send email method from User to EmailUser

--- java-rest-api/src/main/java/com/example/restapi/models/EmailUser.java
+++ java-rest-api/src/main/java/com/example/restapi/models/EmailUser.java
@@ -1,7 +1,87 @@
 package com.example.restapi.models;
 
+import com.sun.mail.util.MailSSLSocketFactory;
+
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+
 public class EmailUser extends User {
 
+    public void notifyUser(String alert){
+        sendEmail(alert);
+    }
+
+    public void sendEmail(String msg) {
+        MailSSLSocketFactory sf = null;
+        try {
+            sf = new MailSSLSocketFactory();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+        sf.setTrustAllHosts(true);
+
+
+        String recipient = getContactInfo();
+
+        // email ID of  Sender.
+        String sender = "teamcodersinc@gmail.com";
+        String password = "codersInc001";
+
+        // using host as localhost
+        String host = "smtp.gmail.com";
+        String port = "587";
+
+        // Getting system properties
+        Properties properties = System.getProperties();
+
+        // Setting up mail server
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+
+        properties.put("mail.imap.ssl.trust", "*");
+        properties.put("mail.imap.ssl.socketFactory", sf);
+        // creating session object to get properties
+        Session session = Session.getDefaultInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(sender, password);
+            }
+        });
+
+        session.getProperties().put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        session.getProperties().put("mail.smtp.starttls.enable", "true");
+
+        try {
+            // MimeMessage object.
+            Message message = new MimeMessage(session);
+
+            // Set From Field: adding senders email to from field.
+            message.setFrom(new InternetAddress(sender));
+
+            // Set To Field: adding recipient's email to from field.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+
+            // Set Subject: subject of the email
+            message.setSubject("Temperature Alert");
+
+            // set body of the email.
+            message.setText(msg);
+
+            // Send email.
+            Transport.send(message);
+            System.out.println("Mail sent successfully");
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
