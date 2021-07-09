7a3ddede5e5ed9ec3a989bb207f4a08ffe40d7d4
sachini <sachiniginige@gmail.com>
2021-07-09 17:57:56 +0530

    edit notifyusers

--- java-rest-api/src/main/java/com/example/restapi/services/TempSensorService.java
+++ java-rest-api/src/main/java/com/example/restapi/services/TempSensorService.java
@@ -30,7 +30,7 @@ public class TempSensorService {
     public List<TempSensor> getSensorsBySensorId(String id){
         return tempSensorRepository.findAllBySensor_id(id)
                 .orElseThrow(()->new RuntimeException(
-                        String.format("Cannot find sensors by Sensor ID: %s",id)
+                    String.format("Cannot find sensors by Sensor ID: %s",id)
                 ));
     }
 
@@ -38,32 +38,29 @@ public class TempSensorService {
         tempSensorRepository.insert(tempSensor);
         if(tempSensor.getData_value()>30.0) {
             String alert="temperature  has exceeded the threshold value ";
-//            newtempSensor.notifyUsers(alert);
             notifyusers(alert);
         }
         System.out.println("Successfully added current reading of Sensor ID: "+tempSensor.getSensor_id());
-
     }
 
-            public void notifyusers(String alert){
-                List<User> users = userService.getAllUsers();
-                for (User user : users) {
-                    if(!user.getContactInfo().isEmpty()){
-                        if(user.getUserType().equals("Email")){
-                            EmailUser emailUser = new EmailUser();
-                            emailUser.setContactInfo(user.getContactInfo());
-                            emailUser.notifyUser(alert);
-                        }else if(user.getUserType().equals("SMS")) {
-                            SMSUser smsUser = new SMSUser();
-                            smsUser.setContactInfo(user.getContactInfo());
-                            smsUser.notifyUser(alert);
-                        }else{
-                            CallUser callUser = new CallUser();
-                            callUser.setContactInfo(user.getContactInfo());
-                            callUser.notifyUser(alert);
-                        }
-                    }
-
+    public void notifyusers(String alert){
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            if(!user.getContactInfo().isEmpty()){
+                if(user.getUserType().equals("Email")){
+                    EmailUser emailUser = new EmailUser();
+                    emailUser.setContactInfo(user.getContactInfo());
+                    emailUser.notifyUser(alert);
+                }else if(user.getUserType().equals("SMS")){
+                    SMSUser smsUser = new SMSUser();
+                    smsUser.setContactInfo(user.getContactInfo());
+                    smsUser.notifyUser(alert);
+                }else if(user.getUserType().equals("Call")){
+                    CallUser callUser = new CallUser();
+                    callUser.setContactInfo(user.getContactInfo());
+                    callUser.notifyUser(alert);
                 }
             }
+        }
+    }
 }
